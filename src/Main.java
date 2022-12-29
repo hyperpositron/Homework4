eb3da44f084abf2807bf697ba8b46416b48c0443
Александр <hyperpositron@gmail.com>
2022-12-29 22:34:47 +0200

    Задача 1

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,13 @@ public class Main {
         //task7();
     }
     public static void task1() {
-        System.out.println(1);
+        //Задача 1
+        System.out.println("Задача №1");
+        int age = 18;
+        if (age >=18)
+        System.out.println("Если возраст человека равен "+age+" , то он совершеннолетний.");
+        if (age < 18)
+        System.out.println("Если возраст человека равен "+age+" , он не достиг совершеннолетия, нужно немного подождать.");
+
     }
 }
