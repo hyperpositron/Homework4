62af8564132ae1454001053d80c83964e54545e8
Александр <hyperpositron@gmail.com>
2022-12-29 23:43:22 +0200

    Задача 4 Задача 3 Задача 2 правки

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
-        //task3();
-        //task4();
+        task3();
+        task4();
         //task5();
         //task6();
         //task7();
@@ -22,13 +22,43 @@ public class Main {
     public static void task2() {
         //Задача 2
         System.out.println("Задача №2");
-        int temperature = 7;
+        int temperature = 10;
         if (temperature < 5) {
-            System.out.println("«На улице холодно, нужно надеть шапку» — если температура ниже 5 градусов.");
+            System.out.println("«На улице холодно, нужно надеть шапку»");
         }
         if (temperature > 5) {
-            System.out.println("«Сегодня тепло, можно идти без шапки» — если температура воздуха выше 5 градусов.");
+            System.out.println("«Сегодня тепло, можно идти без шапки»");
         }
+    }
+    public static void task3() {
+        //Задача 3
+        System.out.println("Задача №3");
+        int carSpeed = 50;
+        if (carSpeed > 60) {
+            System.out.println("Если скорость " + carSpeed + " , то придется заплатить штраф.");
+        }
+        else {
+            System.out.println("Если скорость " + carSpeed + " , можно ездить спокойно.");
+        }
+    }
+    public static void task4() {
+        //Задача 4
+        System.out.println("Задача №4");
+        int agePeople = 24;
+        if (agePeople >= 2 && agePeople <= 6) {
+            System.out.println("Если человеку " + agePeople + " лет, то ему нужно ходить в детский сад.");
+        }
+        if (agePeople >= 7 && agePeople <= 18) {
+            System.out.println("Если человеку " + agePeople + " лет, то ему нужно ходить в школу.");
+        }
+        if (agePeople > 18 && agePeople <= 24) {
+            System.out.println("Если человеку " + agePeople + " лет, то его место в университете.");
+        }
+        if (agePeople > 24)
+            System.out.println("Если человеку " + agePeople + " лет, то ему пора ходить на работу.");
+    }
+    public static void task5() {
 
     }
+
 }
